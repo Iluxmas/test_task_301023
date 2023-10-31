85edea0b1073f3ad7a8e9d1ecdda508deb44e54e
Sulkhanov Ilya <masta.pxty@gmail.com>
2023-10-31 19:34:25 +0300

    feat: request form

---
+++ src/Components/RequestForm/RequestForm.tsx
@@ -0,0 +1,87 @@
+'use client';
+import React, { useState } from 'react';
+import styles from './RequestForm.module.css';
+import Button from '../Button/Button';
+import { InputMask } from '@react-input/mask';
+
+interface RequestFormProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (formData: { name: string; telephone: string }) => void;
+}
+
+function RequestForm({ isOpen, onClose, onSubmit }: RequestFormProps) {
+  const [telephone, setTelephone] = useState('');
+  const [name, setName] = useState('');
+
+  let formClass = isOpen ? [styles.modal, styles.modal_slidedown] : [styles.form];
+
+  const handleSumbit = () => {
+    if (!name || !telephone) {
+      console.log('ставим erorr стили на инпуты');
+      return;
+    }
+    onSubmit({ name, telephone });
+  };
+
+  return (
+    <div className={styles.overlay}>
+      <div className={formClass.join(' ')}>
+        <div className={styles.form__container}>
+          <div className={styles.form__close} onClick={onClose}></div>
+          <p className={styles.form__title}>Онлайн-заявка</p>
+          <p className={styles.form__subtitle}>
+            Заполните форму, и мы вскоре свяжемся с вами, чтобы ответить на все вопросы
+          </p>
+          <form className={styles.form__inputs_container}>
+            <div className={styles.form__input}>
+              <InputMask
+                mask='+7 (XXX) XXX-XX-XX'
+                replacement='X'
+                showMask={true}
+                className={styles.form__input_element}
+                value={telephone ?? ''}
+                onMask={(event) => setTelephone(event.detail.value)}
+              />
+              <span className={styles.form__label}>Телефон *</span>
+            </div>
+            <div className={styles.form__input}>
+              <input
+                type='text'
+                placeholder='Имя'
+                value={name}
+                className={styles.form__input_element}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <span className={styles.form__label}>Имя *</span>
+            </div>
+          </form>
+          <div className={styles.form__agreement_container}>
+            <Button
+              text='Оставить заявку'
+              className={styles.form__submit}
+              onButtonClick={handleSumbit}
+              disabled={!name || !telephone}
+            />
+            <p className={styles.form__agreement_text}>
+              Нажимая на кнопку «Оставить заявку», я даю согласие{' '}
+              <a href='' target='_blank' className={styles.form__agreement_link}>
+                на обработку персональных данных
+              </a>
+            </p>
+          </div>
+          <ul className={styles.socials__list}>
+            <li className={styles.socials__list_item}>
+              <a href='#' className={[styles.social__link, styles.social__item_whatsapp].join(' ')} target='_blank'></a>
+            </li>
+            <li className={styles.socials__list_item}>
+              <a href='#' className={[styles.social__link, styles.social__item_telegram].join(' ')} target='_blank'></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RequestForm;
